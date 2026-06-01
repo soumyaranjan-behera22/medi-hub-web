a193427fcfb97721b1735507d20337bc7745c40e
Soumyaranjan Behera <soumya.rbehera007@gmail.com>
2026-06-01 21:20:42 +0530

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
-// ✅ CORRECT — pass Nitro as a Vite plugin inside the vite.plugins array
+// // ✅ CORRECT — pass Nitro as a Vite plugin inside the vite.plugins array
+// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+// import { nitro } from "nitro/vite";
+
+// export default defineConfig({
+//   tanstackStart: {
+//     server: { entry: "server" },
+//   },
+//   vite: {
+//     plugins: [
+//       nitro({
+//         preset: "vercel",
+//       }),
+//     ],
+//   },
+// });
+
+
+
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
   vite: {
     plugins: [
       nitro({
